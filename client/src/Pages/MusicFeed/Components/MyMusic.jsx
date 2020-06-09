67d71b063be068082b759fe81262b7cf6595a401
Yong <ycho97@uwo.ca>
2020-06-08 21:07:52 -0400

    fourth commit

--- client/src/Pages/MusicFeed/Components/MyMusic.jsx
+++ client/src/Pages/MusicFeed/Components/MyMusic.jsx
@@ -1,14 +1,42 @@
 import React, {Component} from 'react';
 
+import waka from './waka.mp3';
+
 class MyMusic extends Component{
     render(){
         const noSongs=<h1 className='noSongs'>No Music Available</h1>
 
+        const songs=[];
+
+        for(let i=0;i<5;i++){
+            songs.push(
+            <section className='song container'>
+                <h2>Waka<span className='userName'>Gugsa</span></h2>
+
+                <audio controls>
+                    <source src={waka} type='audio/mpeg'/>
+                </audio>    
+
+                <i className="fa trash">&#xf014;</i>
+
+                <section className='likesContainer'>
+                    <i className="fa like">
+                        &#xf087;           
+                    </i>
+
+                    <span className='likesCount'>1000</span>    
+                </section>
+
+                <hr/>
+            </section>
+            )
+        }
+
         return(
             <div className='myMusic'>
-                {noSongs}
+                {songs}
 
-                <section className='newSong'>
+                <section className='addSong'>
                     <label htmlFor='upload'>+</label>
                     <input id='upload' type='file' accept='mp3'/>
                 </section>
